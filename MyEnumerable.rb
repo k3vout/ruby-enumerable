347662200e4eb59fbb03b16d71872048b2d73cf4
Mario Chois <mario.chois.navarro@gmail.com>
2021-09-30 15:01:38 -0500

    Linters: Installed Rubocop and fixed all errors

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -1,25 +1,25 @@
 module MyEnumerable
   def all?(&block)
     result = nil
-    self.each.with_index do |element, idx|
-      result = block.call(element) if idx == 0
-      result = result && block.call(element) if idx != 0
+    each.with_index do |element, idx|
+      result = block.call(element) if idx.zero?
+      result &&= block.call(element) if idx != 0
     end
     result
   end
-  
+
   def any?(&block)
     result = nil
-    self.each.with_index do |element, idx|
-      result = block.call(element) if idx == 0
-      result = result || block.call(element) if idx != 0
+    each.with_index do |element, idx|
+      result = block.call(element) if idx.zero?
+      result ||= block.call(element) if idx != 0
     end
     result
   end
-  
+
   def filter(&block)
     result = []
-    self.each do |element|
+    each do |element|
       result << element if block.call(element) == true
     end
     result
